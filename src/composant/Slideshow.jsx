05c3a69abda09dbfdd0d3d9cb3b6d895afd93f21
Sean Perneckele <sean.perneckele@gmail.com>
2023-10-04 20:35:54 +0200

    Rendu final soutenance

--- src/composant/Slideshow.jsx
+++ src/composant/Slideshow.jsx
@@ -1,87 +1,51 @@
-import '../styles/css/Slideshow.css'
+import React, { useState, useEffect } from 'react';
+import '../styles/css/Slideshow.css';
 import { useParams } from 'react-router-dom';
-import données from '../kasa.json'
-import vector1 from '../asset/Vector1.png'
-import vector2 from '../asset/Vector2.png'
-import { useState, useEffect } from 'react'
-
+import données from '../kasa.json';
+import vector1 from '../asset/Vector1.png';
+import vector2 from '../asset/Vector2.png';
 
 function Slideshow() {
+  const { id } = useParams();
+  const article = données.find((element) => element.id === id);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const { id } = useParams();
-    const article = données.find((element) => element.id === id);  
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-    useEffect(() => {
-        // Masquer la numérotation si une seule image
-        if (article.pictures.length === 1) {
-            const numerotationElement = document.querySelector('.numerotation'); // Utilisez querySelector pour sélectionner par classe
-            if (numerotationElement) {
-                numerotationElement.style.display = 'none';
-            }else{
-                numerotationElement.style.display ='flex' ;
-            }
-        }
-
-        displaySlide();
-    });
-
-    if (!article) {
-      return <div>Logement introuvable</div>;
+  useEffect(() => {
+    // Masquer la numérotation si une seule image
+    if (article.pictures.length === 1) {
+      const numerotationElement = document.querySelector('.numerotation');
+      if (numerotationElement) {
+        numerotationElement.style.display = 'none';
+      }
     }
+  }, [article.pictures.length]);
 
-         // Fonction pour passer à l'image précédente
-    const previousImage = () => {
-        if (currentImageIndex > 0) {
-            setCurrentImageIndex(currentImageIndex - 1);
-        } else {
-            setCurrentImageIndex (article.pictures.length - 1);
-        }
-    };
+  if (!article || !article.pictures || article.pictures.length === 0) {
+    return <div>Logement introuvable</div>;
+  }
 
+  // Fonction pour passer à l'image précédente
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? article.pictures.length - 1 : prevIndex - 1));
+  };
 
-    
-    // Fonction pour passer à l'image suivante
-    const nextImage = () => {
-        if (currentImageIndex < article.pictures.length - 1) {
-            setCurrentImageIndex(currentImageIndex + 1);
-            
-        } else {
-            setCurrentImageIndex(0);
-        }
-    };
-  
+  // Fonction pour passer à l'image suivante
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === article.pictures.length - 1 ? 0 : prevIndex + 1));
+  };
 
-    const displaySlide = () => {
-        // Récupérez la diapositive actuelle en fonction de l'index courant
-        const slide = article.pictures[currentImageIndex];
-    
-        // Sélectionnez l'élément HTML où vous souhaitez afficher l'image (par exemple, un élément avec la classe "slide-image")
-        const imageElement = document.querySelector(".slideshow div");
-     
-    
-        // Mettez à jour le style de l'élément image pour afficher la diapositive actuelle
-        if (imageElement && slide) {
-            imageElement.style.backgroundImage = `url(${slide})`;
-        }
-    
-      
-    };
-
-    return (
-        <div className='slideshow'>
-            <div> 
-                <img onClick={previousImage} className='vector1' src={vector1} alt="flèche gauche" />
-                <div className='numerotation'>
-                    {/* Affichez l'index de l'image actuelle */}
-                    {currentImageIndex + 1} / {article.pictures.length}
-                </div>
-                <img onClick={nextImage} className='vector2'  src={vector2} alt="flèche droite" />
-            </div>
+  return (
+    <div className='slideshow'>
+      <div className='slide-image'>
+        <img src={article.pictures[currentImageIndex]} alt={`Slide ${currentImageIndex}`} />
+        <img onClick={previousImage} className='arrow vector1' src={vector1} alt="flèche gauche" />
+        <div className='numerotation'>
+          {currentImageIndex + 1} / {article.pictures.length}
         </div>
-
-    )
-
+        <img onClick={nextImage} className='arrow vector2' src={vector2} alt="flèche droite" />
+      </div>
+    </div>
+  );
 }
 
-export default Slideshow
+export default Slideshow;
--- src/composant/Thumbnail.jsx
+++ src/composant/Thumbnail.jsx
@@ -2,18 +2,19 @@ import données from '../kasa.json'
 import '../styles/css/Thumbnail.css'
 import { Link } from 'react-router-dom'
 
-
 function Thumbnail() {
-    return (
+  return (
     <div className='thumbnail'>
-    {données.map((article) => (
-        
-        <Link  to={`/logement/${article.id}`} className='thumbnail__article'  key={article.id} style={{ backgroundImage: `url(${article.cover})` }}>
-          <p>{ article.title } </p> 
+      {données.map((article) => (
+        <Link to={`/logement/${article.id}`} className='thumbnail__article' key={article.id}>
+          <div className="thumbnail__image-container">
+            <img src={article.cover} alt="" />
+          </div>
+          <p>{article.title}</p>
         </Link>
-    ))}
- </div>
+      ))}
+    </div>
   )
 }
 
-export default Thumbnail
+export default Thumbnail;
